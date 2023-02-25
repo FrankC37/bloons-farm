fc675aeefb6b01e935dd85ffae39cfa0e85294a7
Frank Coniglio <fchammer15@gmail.com>
2023-02-25 10:25:14 -0500

    cleaned up comments

--- infernoFarm.py
+++ infernoFarm.py
@@ -2,7 +2,13 @@ import pyautogui
 import time
 
 #to do; 
-# add logic for level gains mid run, needs screen coords 
+# add logic for level gains mid run
+
+#assumptions
+    # set alch keybind to "A"
+    # set village keybind to "V"
+    # set sniper keybind to "S"
+    # tower upgrade hotkeys are default "," "." "/"
 
 # Screen Size =  1440 x 900
 #buffer time to swap to bloons
@@ -15,31 +21,27 @@ while True:
     # play button = 616,771
     pyautogui.click(616,771)
 
-    # # expert map icon = 1031,812
+    #expert map icon = 1031,812
     pyautogui.click(1031,812)
 
-    # # inferno map = 716,471
+    #inferno map = 716,471
     pyautogui.click(716,471)
 
 
-    # # easy diff = 432,337
+    # easy diff = 432,337
     pyautogui.click(432,337)
 
-    # # deflation mode = 975, 361
+    # deflation mode = 975, 361
     pyautogui.click(975,361)
     time.sleep(7)
 
-    #okay the popup
+    # okay the popup for delflation
     pyautogui.click(704,621)
 
-
-    ### towers
+    ### tower placements
     ## Hot Keys Village = V, Sniper = S, Alch = A
 
-
-    # village = 1183,543
-    # place "V" and send upgrade commands
-    # // && ,,
+    # village placement 2-0-2
     pyautogui.moveTo(1178,521)
     pyautogui.press('V',presses=4)
     pyautogui.click()
@@ -47,9 +49,7 @@ while True:
     pyautogui.press("/",presses=2)
     pyautogui.press(",",presses=2)
 
-    # sniper = 1154,437
-    # place "S" and send upgrade commands
-    # .. & ////
+    # sniper placement 2-0-4
     pyautogui.moveTo(1154,437)
     pyautogui.press('S',presses=4)
     pyautogui.click()
@@ -57,9 +57,7 @@ while True:
     pyautogui.press(".",presses=2)
     pyautogui.press("/",presses=4)
 
-    # alch = 1207,436
-    # place "A" and send upgrade commands
-    # ,,,, && ..
+    # alchemist placement 4-2-0
     pyautogui.moveTo(1207,436)
     pyautogui.press('A',presses=4)
     pyautogui.click()
@@ -68,18 +66,17 @@ while True:
     pyautogui.press(".",presses=2)
 
     # run map and set to fast speed
-    # play button = 1375,806 (two clicks)
     pyautogui.click(1375,806,clicks=2,interval=1.0)
 
     # add logic for level up popup clear 
     # two clicks in middle of screen
 
-    # runtime is approx 295 seconds
+    # runtime is approx 295 seconds, time added in for a buffer
     time.sleep(310)
-    # exit game = 710,775
+    # clicking exit game
     pyautogui.click(710,775)
     time.sleep(3)
 
-    # home = 517,708
+    # clicking home button
     pyautogui.click(517,708)
     time.sleep(3)
