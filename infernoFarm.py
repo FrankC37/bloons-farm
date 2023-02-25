f97f8cead0811a574d8a7aad52e7f85459f96aea
Frank Coniglio <fchammer15@gmail.com>
2023-02-25 17:56:52 -0500

    checks resolution before running

--- infernoFarm.py
+++ infernoFarm.py
@@ -1,81 +1,76 @@
 import pyautogui
 import time
+from threading import Timer
 
-#to do; 
-# add logic for level gains mid run
+screenSizeX, screenSizeY = pyautogui.size()
 
-#assumptions
-    # set alch keybind to "A"
-    # set village keybind to "V"
-    # set sniper keybind to "S"
-    # tower upgrade hotkeys are default "," "." "/"
-    # Screen Size =  1440 x 900
+if screenSizeX != 1440 and screenSizeY != 900:
+    print('Resolution is Incorrect for this scripts parameters, you will need to make customizations to make this fit your resolution.')
+    print(f"Your screen resolution is {screenSizeX},{screenSizeY}")
+    exit()
+else:
+    pyautogui.PAUSE = 0.5
+    #define tower placement parameters
+    def sniper():
+        pyautogui.moveTo(1154,437)
+        pyautogui.press('S',presses=4)
+        pyautogui.click(clicks=2,interval=0.5)
+        pyautogui.press(".",presses=2)
+        pyautogui.press("/",presses=4)
 
-pyautogui.PAUSE = 0.5
-#define tower placement parameters
-def sniper():
-    pyautogui.moveTo(1154,437)
-    pyautogui.press('S',presses=4)
-    pyautogui.click(clicks=2,interval=0.5)
-    pyautogui.press(".",presses=2)
-    pyautogui.press("/",presses=4)
+    def village():
+        pyautogui.moveTo(1178,521)
+        pyautogui.press('V',presses=4)
+        pyautogui.click(clicks=2,interval=0.5)
+        pyautogui.press("/",presses=2)
+        pyautogui.press(",",presses=2)
 
-def village():
-    pyautogui.moveTo(1178,521)
-    pyautogui.press('V',presses=4)
-    pyautogui.click(clicks=2,interval=0.5)
-    pyautogui.press("/",presses=2)
-    pyautogui.press(",",presses=2)
+    def alchemist():
+        pyautogui.moveTo(1207,436)
+        pyautogui.press('A',presses=4)
+        pyautogui.click(clicks=2,interval=0.5)
+        pyautogui.press(",",presses=4)
+        pyautogui.press(".",presses=2)
 
-def alchemist():
-    pyautogui.moveTo(1207,436)
-    pyautogui.press('A',presses=4)
-    pyautogui.click(clicks=2,interval=0.5)
-    pyautogui.press(",",presses=4)
-    pyautogui.press(".",presses=2)
+    #define functions for game controls
+    def startMap():
+        # play button
+        pyautogui.click(616,771)
+        #expert map icon
+        pyautogui.click(1031,812)
+        #inferno map
+        pyautogui.click(716,471)
+        # easy diff
+        pyautogui.click(432,337)
+        # deflation mode
+        pyautogui.click(975,361)
+        time.sleep(4)
+        # clear deflation popup
+        pyautogui.click(704,621)
 
-#define functions for game controls
-def startMap():
-    # play button
-    pyautogui.click(616,771)
-    #expert map icon
-    pyautogui.click(1031,812)
-    #inferno map
-    pyautogui.click(716,471)
-    # easy diff
-    pyautogui.click(432,337)
-    # deflation mode
-    pyautogui.click(975,361)
-    time.sleep(7)
-    # clear deflation popup
-    pyautogui.click(704,621)
+    def runMap():
+        pyautogui.click(1375,806,clicks=2,interval=1.0)
 
-def runMap():
-    pyautogui.click(1375,806,clicks=2,interval=1.0)
+    def endOfMatch():
+        pyautogui.click(710,775)
+        pyautogui.click(517,708)
 
-def endOfMatch():
-    pyautogui.click(710,775)
-    pyautogui.click(517,708)
+    sleepDuringMatch = Timer(297, endOfMatch) 
 
-#buffer time to swap to bloons window
-time.sleep(5)
+    #buffer time to swap to bloons window
+    time.sleep(5)
 
-while True:
-    time.sleep(3)
-    
-    startMap()
+    while True:
+        time.sleep(3)
 
-    # tower placements
-    village()
-    sniper()
-    alchemist()
+        startMap()
 
-    runMap()
+        # tower placements
+        village()
+        sniper()
+        alchemist()
 
-    # add logic for level up popup clear 
-    # two clicks in middle of screen
+        runMap()
 
-    # runtime is approx 295 seconds, time added in for a buffer
-    time.sleep(295)
-
-    endOfMatch()
+        sleepDuringMatch.start()
+        pyautogui.click(700,450,interval=1,clicks=290)
